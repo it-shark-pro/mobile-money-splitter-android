6431c40b9cdd71b8701731d5fc13fd817316bbf1
Mikhail_Kantsavoi <mikhail_kantsavoi@epam.com>
2018-05-08 11:29:11 +0300

    First commit

---
+++ app/src/main/java/pro/itshark/moneysplitter/EventsActivity.kt
@@ -0,0 +1,12 @@
+package pro.itshark.moneysplitter
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class EventsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_events)
+    }
+}
